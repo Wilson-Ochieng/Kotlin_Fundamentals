0255cb7ededcd94057544ef374353691d7349c2b
Wilson <wilsonochieng718@gmail.com>
2024-08-31 12:49:50 +0300

    Extending interfaces

--- src/GenericDataType.kt
+++ src/GenericDataType.kt
@@ -9,15 +9,26 @@ fun printProgressBar()
 
 }
 
-class   Quiz {
+class   Quiz : ProgressPrintable {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.Medium)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.Easy)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.Hard)
 //Test variable
 
+    override  val progressText:String
+    get()= "${answered} of ${total}"
+
+   override fun printProgressBar(){
+       repeat(Quiz.total - Quiz.answered){ println("▓") }
+       println()
+       println(progressText)
+   }
+
+
 
 
     companion object StudentProgress{
+
 
         val test: String= "Test"
         var  total:Int = 10
@@ -30,18 +41,18 @@ class   Quiz {
 
 }
 //Extension property
-val Quiz.StudentProgress.progressText: String
-    get()= "${answered} of ${total}"
+//val Quiz.StudentProgress.progressText: String
+//    get()= "${answered} of ${total}"
 
 
 //Extension function
-fun Quiz.StudentProgress.printProgressBar() {
-
-    repeat(Quiz.total - Quiz.answered){ println("▓") }
-    println()
-    println(Quiz.progressText)
-
-}
+//fun Quiz.StudentProgress.printProgressBar() {
+//
+//    repeat(Quiz.total - Quiz.answered){ println("▓") }
+//    println()
+//    println(Quiz.progressText)
+//
+//}
 
 
 
@@ -58,6 +69,5 @@ data class Question <T> (
 
 fun main() {
 
-println(Quiz.printProgressBar())
-
+    Quiz().printProgressBar()
 }
